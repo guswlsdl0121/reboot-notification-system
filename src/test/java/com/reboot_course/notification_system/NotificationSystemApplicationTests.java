e2d794d2384aec1c04aed55deda6d9ff0438282e
íguswlsdl0121 <guswlsdl0121@gmail.com>
2024-10-12 17:19:44 +0900

    build: Spring initializer 초기 세팅 및 도커 세팅

---
+++ src/test/java/com/reboot_course/notification_system/NotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reboot_course.notification_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
