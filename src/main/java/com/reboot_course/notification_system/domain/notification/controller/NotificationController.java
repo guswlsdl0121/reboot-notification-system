cc7dd02cf69a41e11cea1338f2a08f8430d6ee1e
íguswlsdl0121 <guswlsdl0121@gmail.com>
2024-10-14 11:48:20 +0900

    feat: NotificationController 추가
    - 간략하게 호출부 작성

---
+++ src/main/java/com/reboot_course/notification_system/domain/notification/controller/NotificationController.java
@@ -0,0 +1,29 @@
+package com.reboot_course.notification_system.domain.notification.controller;
+
+import com.reboot_course.notification_system.domain.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+
+    @PostMapping("/{productId}/notifications/re-stock")
+    public ResponseEntity<String> sendNotification(@PathVariable Long productId) {
+        // TODO: 재입고 알림 전송 로직 구현
+        notificationService.sendNotifications(productId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/admin/{productId}/notifications/re-stock")
+    public ResponseEntity<String> resendNotification(@PathVariable Long productId) {
+        // TODO: 관리자용 재입고 알림 재전송 로직 구현
+        // notificationService.resendRestockNotification(productId);
+        return ResponseEntity.ok().build();
+    }
+}
