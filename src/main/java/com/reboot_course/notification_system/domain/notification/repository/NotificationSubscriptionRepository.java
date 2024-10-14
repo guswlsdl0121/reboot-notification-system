26a1cc94a962bf64f5206eab1ad4114d5cccd59b
íguswlsdl0121 <guswlsdl0121@gmail.com>
2024-10-14 11:50:46 +0900

    feat: product 및 알림 등록 리포지토리 정의

---
+++ src/main/java/com/reboot_course/notification_system/domain/notification/repository/NotificationSubscriptionRepository.java
@@ -0,0 +1,12 @@
+package com.reboot_course.notification_system.domain.notification.repository;
+
+import com.reboot_course.notification_system.domain.subscription.entity.NotificationSubscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationSubscriptionRepository extends JpaRepository<NotificationSubscription, Long> {
+    List<NotificationSubscription> findAllByProductIdOrderByCreatedAtAsc(Long productId);
+}
